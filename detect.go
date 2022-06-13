52e8bdaf9e3a40807a0ae6388efa394c739e12dc
Zachary Yedidia <zyedidia@gmail.com>
2022-06-13 21:11:26 +0100

    Add CustomClipName

--- detect.go
+++ detect.go
@@ -15,6 +15,14 @@ func (me MultiErr) Error() string {
 	return b.String()
 }
 
+// CustomClipName is the name of the first executable that GetClipboard will
+// search for, allowing applications to support users providing their own
+// clipboard support via a custom script.  The script will be called as
+// `<name> -i <register>` for copy and `<name> -o <register>` for paste. The
+// text to copy will be provided by stdin and clipper expects the paste text
+// to be printed to stdout.
+var CustomClipName = "myclip"
+
 // GetClipboard attempts to find a valid clipboard and returns it. It first
 // checks for a custom `myclip` script, and then for pb, xclip, xsel, wayland,
 // wsl, and termux. If nothing is found, a list of errors is returned along
@@ -23,7 +31,7 @@ func GetClipboard() (clip Clipboard, err error) {
 	var errs MultiErr
 
 	clip = &Custom{
-		Name: "myclip",
+		Name: CustomClipName,
 	}
 	if err = clip.Init(); err == nil {
 		return clip, nil
